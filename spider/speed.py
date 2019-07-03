bf1a61b03e8e32f1c68d6555a816716d00bc4e8b
yongjie.zhang@ucarinc.com <yongjie.zhang@ucarinc.com>
2019-07-03 14:49:22 +0800

    初步完善

--- spider/speed.py
+++ spider/speed.py
@@ -5,6 +5,7 @@ import json
 import os
 import urllib
 from PIL import Image
+import threading
 #爬取温州大学首页的报道
 #爬取温州大学首页的报道
 def getRootPath():
@@ -25,81 +26,80 @@ def get_door_web():
     res = requests.get(door_url)
     # print(res.text)
     d = pq(res.text)
-    a_list = d('#js_content > table > tbody > tr > td > p > a')
-    # a_list = d('#js_content > table')
+    a_list = d('#js_content > table > tbody > tr > td > p > a') + d('#js_content > table > tbody > tr > td  > a')
+
     url_list = []
+    span_list = []
+
+    span_url_dic = {}
     for i, item in enumerate(a_list):
-        url_list.append(item.get('href'))
+        span_url_dic[item.find('strong').find('span').text if not item.find('strong')is None else
+                         item.find('span').find('span').find('strong').text] = item.get('href')
+        # span_list.append(item.find('strong').find('span').text if not item.find('strong')is None else
+        #                  item.find('span').find('span').find('strong').text)
+        # # span_list.append(item.find('strong'))
+        #
+        # url_list.append(item.get('href'))
+
     # print(url_list)
-    return url_list
+    return span_url_dic
 
 
 
-def get_panpan_web():
-    # 防止反爬机制检测 user-agent
-    # header = {
-    #     "token":"76f43218d868401b9204112217b102ee",
-    #
-    # "User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)
-    #  Chrome/74.0.3729.169 Safari/537.36",
-    #
-    # }
+# def get_panpan_web():
+#     # 防止反爬机制检测 user-agent
+#     # header = {
+#     #     "token":"76f43218d868401b9204112217b102ee",
+#     #
+#     # "User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)
+#     #  Chrome/74.0.3729.169 Safari/537.36",
+#     #
+#     # }
+#
+#     resp = requests.get(url)
+#     print(type(resp))
+#     print(resp.text)
 
-    resp = requests.get(url)
-    print(type(resp))
-    print(resp.text)
-
-
-def get_web(url_list):
-    resp_list = []
-    for temp in url_list:
-        resp = requests.get(temp)
-        resp.encoding = ('utf-8')
-        resp_list.append(resp.text)
-
-    print(resp_list)
-
-    return resp_list
+def get_web_info(each_url):
+    resp = requests.get(each_url)
+    resp.encoding = ('utf-8')
+    return resp.text
 
 
-def pq_web(html):
+def spider_info(span_url_dic):
+    for k, v in span_url_dic.items():
+        # t = threading.Thread(target=execute_begin, args=(k, v,), name="thread-{}".format(k))
+        # t.start()
+        execute_begin(k, v)
+
+
+def execute_begin(span_name, each_url):
+    html = get_web_info(each_url)
     d = pq(html)
-    piclist = []
+    pic_list = d('#js_content > p > img')
+    # 删除开始图片和最后两张图片
+    # pic_list.pop(0)
+    # pic_list.pop()
+    # pic_list.pop()
+    img_list = []
     byte_list = []
-    for i in range(7,21):
-        title = d('#js_content > p:nth-child({}) > img'.format(i))
-        piclist.append(title)
-    imgList = []
-    for i, item in enumerate(piclist):
-        print(item.attr("data-src"), i)
-        urllib.request.urlretrieve(item.attr("data-src"), './zhang{}.png'.format(i))
-        imgList.append('./zhang{}.png'.format(i))
-
-    img1 = Image.open(imgList[0])
-    imgList.pop(0)
-    for t in imgList:
+    for i, item in enumerate(pic_list):
+        urllib.request.urlretrieve(item.get("data-src"), '../image/{}{}.png'.format(span_name, i))
+        img_list.append('../image/{}{}.png'.format(span_name, i))
+    img1 = Image.open(img_list[0])
+    img_list.pop(0)
+    for t in img_list:
         img = Image.open(t)
         byte_list.append(img)
-    img1.save('一人之下万人之上.pdf', "PDF", resolution=100.0, save_all=True, append_images=byte_list)
-    # filename = rootpath + "\\token.txt"
-    # with open(filename, 'w') as f:
-    #     f.write(title.attr("data-src"))
-    # #print(title.attr("data-src"))
-    #
-    # with open(filename, 'r') as fd:
-    #     line = fd.readline()
-    #     print(line)
-    #
-    # urllib.request.urlretrieve(line, './img1.png')
-    # data = requests.get(line)
-    # with open(rootpath + "/zhang.jpg", 'w') as t:
-    #     t.write(data.content)
+    img1.save('../query/{}.pdf'.format(span_name), "PDF", resolution=100.0, save_all=True, append_images=byte_list)
 
 
 def main():
-    url_list = get_door_web()
-    resp_list = get_web(url_list)
-    print(resp_list)
+    span_url_dic = get_door_web()
+    print(len(span_url_dic))
+    spider_info(span_url_dic)
+    # resp_list = get_web(url_list)
+    # print(resp_list)
     # pq_web(resp)
 
 
